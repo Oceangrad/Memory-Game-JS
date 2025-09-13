c96c7b07626b20d0f1c5ad69cea4b86a65706623
Oceangrad <peterportal112@gmail.com>
2025-09-13 12:17:42 +0300

    repo publish

---
+++ main.js
@@ -0,0 +1,136 @@
+const FIELD = document.getElementById("field");
+const CONFIG = {
+    fieldSize: {
+        x: 7,
+        y: 6
+    },
+    cellSize: {
+        x: "60px",
+        y: "60px"
+    },
+    closedCellColor: "black",
+    timeout: 500,
+    selectedStyle: "3px red solid"
+};
+
+init();
+
+let cells = FIELD.getElementsByTagName("td");
+let colors = fillColorArray();
+
+let first = true;
+
+let canOpen = true;
+
+let elem1 = null;
+let elem2 = null;
+
+let closedElems = [];
+
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", function() { open(i); });
+}
+
+//----------------------------------GAME INITIALIZING-------------------------
+
+function init() {
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+    td.style.width = CONFIG.cellSize.x;
+    td.style.height = CONFIG.cellSize.y;
+    td.style.backgroundColor = CONFIG.closedCellColor;
+
+    for (let i = 0; i < CONFIG.fieldSize.x; i++) {
+        tr.innerHTML += td.outerHTML;
+    }
+
+    for (let i = 0; i < CONFIG.fieldSize.y; i++) {
+        FIELD.innerHTML += tr.outerHTML;
+    }
+}
+
+function fillColorArray() {
+    let colors = [];
+    let even = CONFIG.fieldSize.x*CONFIG.fieldSize.y % 2;
+    if (even !== 0) throw new Error("Number of cell should be even.");
+
+    for (let i = 0; i < (CONFIG.fieldSize.x*CONFIG.fieldSize.y)/2; i++) {
+        let color = getRandColor();
+        colors.push(color, color);
+    }
+    shuffle(colors);
+    return colors;
+}
+
+function getRandColor() {
+    let color = {
+        r: getRandNum(0, 255),
+        g: getRandNum(0, 255),
+        b: getRandNum(0, 255)
+    }
+
+    color.toCSS = () => { return "rgb("+color.r+","+color.g+","+color.b+")"; };
+    color.equal = (value) => {
+        return color.r==value.r && color.g==value.g && color.b==value.b;
+    }
+
+    return color;
+}
+
+function getRandNum(start, end) {
+    let num = (Math.random()*(end-start))+start;
+    num = Math.round(num);
+    return num;
+}
+
+// Спиздил 0w0
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+//-------------------------------------------------GAME PROCESS-------------------------------------------------
+
+function open(i) {
+    let cell = cells[i];
+
+    if (closedElems.findIndex((val) => val == i) !== -1 || !canOpen) return;
+
+    if (first) {
+        cell.style.backgroundColor = colors[i].toCSS();
+        cell.style.border = CONFIG.selectedStyle;
+        elem1 = i;
+    }
+    else {
+        if (elem1 == i) {
+            return;
+        }
+
+        cell.style.backgroundColor = colors[i].toCSS();
+        cell.style.border = CONFIG.selectedStyle;
+        elem2 = i;
+
+        let color1 = colors[elem1];
+        let color2 = colors[elem2];
+
+        let cell1 = cells[elem1];
+        let cell2 = cells[elem2];
+
+        canOpen = false;
+        setTimeout(() => {
+            if (color1.equal(color2)) {
+                closedElems.push(elem1, elem2);
+                if (closedElems.length == cells.length) {
+                    console.log("win");
+                }
+            } else {
+                cell1.style.backgroundColor = CONFIG.closedCellColor;
+                cell2.style.backgroundColor = CONFIG.closedCellColor;
+            }
+            cell1.style.border = "";
+            cell2.style.border = "";
+            canOpen = true;
+        }, CONFIG.timeout);
+    }
+
+    first = !first;
+}
